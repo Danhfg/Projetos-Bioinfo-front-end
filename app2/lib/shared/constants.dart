7c457a9741c85a88d6f81a52e3bf5377b244029f
Daniel Henrique <danielhenriquefg@gmail.com>
2023-05-17 23:32:04 -0300

    Update constants.dart

--- app2/lib/shared/constants.dart
+++ app2/lib/shared/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const BASE_URL = "https://bioinfo.imd.ufrn.br/daniel_backend";
-// const BASE_URL = "http://localhost:8443";
+// const BASE_URL = "https://bioinfo.imd.ufrn.br/daniel_backend";
+const BASE_URL = "http://10.7.43.13:8443";
 
 const kPrimaryColor = Colors.lightBlue;
 const kTextColor = Color(0xFF3C4046);
